47180fbe71bc7a8f20d51ea358ad118367d3627c
VedanteePathak <pathakvedantee5@gmail.com>
2024-05-01 23:38:15 +0530

    Add missing files

--- peerpulse-frontend/src/app/app.component.ts
+++ peerpulse-frontend/src/app/app.component.ts
@@ -1,13 +1,42 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { AuthenticationService } from './_services/authentication.service';
+import { Observable, catchError, of } from 'rxjs';
+import { HttpErrorResponse } from '@angular/common/http';
+import { GlobalService } from './_services/global.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'peerpulse-frontend';
+export class AppComponent implements OnInit {
+  title = 'PeerPulse-frontend';
+  constructor(
+    private authService: AuthenticationService,
+    private globalService: GlobalService,
+    private router: Router
+  ) {
+
+  }
+
+  ngOnInit(): void {
+    this.authService.sessionAlive()
+      .pipe(
+        catchError(
+          (err: HttpErrorResponse) => {
+            console.log(err);
+            return of(false);
+          }
+        )
+      ).subscribe(
+        (data: boolean | String) => {
+          if (data === false && this.globalService.currentCredentials) {
+            alert("The session has expired, please log in again.")
+            this.globalService.eraseCredentials();
+            this.router.navigate(['/']);
+          }
+        }
+      );
+  }
 }
